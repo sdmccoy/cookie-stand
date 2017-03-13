ded0721b6b9f5523daaa85ff0d611e66df9097af
Scott <sdmccoy@gmail.com>
2017-03-13 16:55:32 -0700

    Made the 5 objects

---
+++ app.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var cookieStand = {
+  name: '1st and Pike',
+  minHrlyCustomers = ,
+  maxHrlyCustomers = ,
+  avgCookiesPerCust = ,
+  var CustomersPerHr = function() {
+    return Math.floor(Math.random() * (maxHrlyCustomers - minHrlyCustomers + 1)) + minHrlyCustomers;
+  }
+  var cookiesPerHr =  function() {
+    return CustomersPerHr * avgCookiesPerCust;
+  }
+}
+
+var cookieStand = {
+  name: 'SeaTac Airport',
+  minHrlyCustomers = ,
+  maxHrlyCustomers = ,
+  avgCookiesPerCust = ,
+  var CustomersPerHr = function() {
+    return Math.floor(Math.random() * (maxHrlyCustomers - minHrlyCustomers + 1)) + minHrlyCustomers;
+  }
+  var cookiesPerHr =  function() {
+    return CustomersPerHr * avgCookiesPerCust;
+  }
+}
+
+var cookieStand = {
+  name: 'Seattle Center',
+  minHrlyCustomers = ,
+  maxHrlyCustomers = ,
+  avgCookiesPerCust = ,
+  var CustomersPerHr = function() {
+    return Math.floor(Math.random() * (maxHrlyCustomers - minHrlyCustomers + 1)) + minHrlyCustomers;
+  }
+  var cookiesPerHr =  function() {
+    return CustomersPerHr * avgCookiesPerCust;
+  }
+}
+
+var cookieStand = {
+  name: 'Capitol Hill',
+  minHrlyCustomers = ,
+  maxHrlyCustomers = ,
+  avgCookiesPerCust = ,
+  var CustomersPerHr = function() {
+    return Math.floor(Math.random() * (maxHrlyCustomers - minHrlyCustomers + 1)) + minHrlyCustomers;
+  }
+  var cookiesPerHr =  function() {
+    return CustomersPerHr * avgCookiesPerCust;
+  }
+}
+
+var cookieStand = {
+  name: 'Alki',
+  minHrlyCustomers = ,
+  maxHrlyCustomers = ,
+  avgCookiesPerCust = ,
+  var CustomersPerHr = function() {
+    return Math.floor(Math.random() * (maxHrlyCustomers - minHrlyCustomers + 1)) + minHrlyCustomers;
+  }
+  var cookiesPerHr =  function() {
+    return CustomersPerHr * avgCookiesPerCust;
+  }
+}
