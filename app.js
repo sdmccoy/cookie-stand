379889bfe82122db047405b704c768b8cc505085
Scott McCoy <Scott McCoy>
2017-03-13 13:44:42 -0700

    Changed to literal notation form

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 'use strict';
 
-var locations = {
-  var maxCustomers = maxCustomers,
-  var minCustomers = minCustomers,
-  var avgCookiePerCust = avgCookiePerCust
+var cookieStore = {
+  maxHrlyCustomer = ,
+  minHrlyCustomer = ,
+  avgCookiesPerCust = ,
+  customersPerHr = function(){
+    Math.floor(Math.random()*100);
+  }
+  cookiesPerHr = function(){
+    avgCookiesPerCust * customersPerHr;
+  }
 }
