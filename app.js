a20e8b46ebb7a726f79ba54fafb8f01293a6328c
Scott <sdmccoy@gmail.com>
2017-03-14 15:52:58 -0700

    table is working and format is correct. Could work on some CSS

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var storeHrs = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var storeHrs = ['', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 var body = document.getElementsByTagName('body')[0];
 
 function cookieStand(name, minHrlyCustomers, maxHrlyCustomers, avgCookiesPerCust) {
@@ -10,6 +10,7 @@ function cookieStand(name, minHrlyCustomers, maxHrlyCustomers, avgCookiesPerCust
   this.avgCookiesPerCust = avgCookiesPerCust;
   this.total = 0;
   this.salesArr = [];
+  this.storeName = [];
   this.customersPerHr = function() {
     return Math.floor(Math.random() * (this.maxHrlyCustomers - this.minHrlyCustomers + 1) + this.minHrlyCustomers);
     console.log(customersPerHr);
@@ -17,29 +18,29 @@ function cookieStand(name, minHrlyCustomers, maxHrlyCustomers, avgCookiesPerCust
 
   this.dailyCookies = function() {
     this.customersPerHr();
-    for (var i = 0; i < storeHrs.length; i++) {
+    for (var i = 0; i < storeHrs.length - 1; i++) {
       var cookiesPerHr = Math.floor(this.customersPerHr() * this.avgCookiesPerCust);
       this.salesArr.push(cookiesPerHr);
       this.total += cookiesPerHr;
     }
   };
 
-  this.createListItems = function() {
+  this.tableData = function() {
     this.dailyCookies();
-    // // tableHead.innerText = this.name;
-    // for (var i = 0; i < storeHrs.length; i++) {
-    //   var tableRow = document.createElement('tr');
-    //   tableRow.innerText = storeHrs[i];
-    //   thead.appendChild(tr);
-    // }
-  //   var firstUL = document.createElement('ul');
-  //   for (var i = 0; i < storeHrs.length; i++) {
-  //     var newEl = document.createElement('li');
-  //     newEl.innerText = storeHrs[i] + ': ' + this.salesArr[i] + ' cookies.';
-  //     firstUL.appendChild(newEl);
-  //   }
-  //   body.appendChild(firstUL);
-  // };
+    var table = document.getElementsByTagName('table')[0];
+    var tableBody = document.createElement('tbody');
+    table.appendChild(tableBody);
+    var tRow = document.createElement('tr');
+    tableBody.appendChild(tRow);
+    var nameTD = document.createElement('td');
+    nameTD.innerText = this.name;
+    tRow.appendChild(nameTD);
+    for (var i = 0; i < storeHrs.length - 1; i++) {
+      var newTD = document.createElement('td');
+      newTD.innerText = this.salesArr[i];
+      tRow.appendChild(newTD);
+    }
+    // body.appendChild(tRow);
   };
 }
 
@@ -57,6 +58,7 @@ function createTable() {
   }
 };
 createTable();
+// tableData();
 
 var oneAndPike = new cookieStand('1st and Pike', 23, 65, 6.3);
 var seaTacAirport = new cookieStand('SeaTac Airport', 3, 24, 1.2);
@@ -64,11 +66,12 @@ var seattleCenter = new cookieStand('Seattle Center', 11, 38, 3.7);
 var capitolHill = new cookieStand('Capitol Hill', 20, 38, 2.3);
 var alki = new cookieStand('Alki', 2, 16, 4.6);
 
-oneAndPike.createListItems();
-seaTacAirport.createListItems();
-seattleCenter.createListItems();
-capitolHill.createListItems();
-alki.createListItems();
+oneAndPike.tableData();
+seaTacAirport.tableData();
+seattleCenter.tableData();
+capitolHill.tableData();
+alki.tableData();
+
 //
 // }
 //
