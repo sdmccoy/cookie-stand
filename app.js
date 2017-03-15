f3be871d2a75a7ab2b80f3f5f083f8dfae2106e9
Scott <sdmccoy@gmail.com>
2017-03-15 13:13:11 -0700

    Made scaling adjustments for the stores. When adding a new store, it will push the methods in to the allstores array. At the bottom a for loop runs to get the tabledata from each store.

--- app.js
+++ app.js
@@ -2,14 +2,15 @@
 
 var storeHrs = ['', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 var body = document.getElementsByTagName('body')[0];
+var allStores = [];
 
-var oneAndPike = new cookieStand('1st and Pike', 23, 65, 6.3);
-var seaTacAirport = new cookieStand('SeaTac Airport', 3, 24, 1.2);
-var seattleCenter = new cookieStand('Seattle Center', 11, 38, 3.7);
-var capitolHill = new cookieStand('Capitol Hill', 20, 38, 2.3);
-var alki = new cookieStand('Alki', 2, 16, 4.6);
+var oneAndPike = new CookieStand('1st and Pike', 23, 65, 6.3);
+var seaTacAirport = new CookieStand('SeaTac Airport', 3, 24, 1.2);
+var seattleCenter = new CookieStand('Seattle Center', 11, 38, 3.7);
+var capitolHill = new CookieStand('Capitol Hill', 20, 38, 2.3);
+var alki = new CookieStand('Alki', 2, 16, 4.6);
 
-function cookieStand(name, minHrlyCustomers, maxHrlyCustomers, avgCookiesPerCust) {
+function CookieStand(name, minHrlyCustomers, maxHrlyCustomers, avgCookiesPerCust) {
   this.name = name;
   this.minHrlyCustomers = minHrlyCustomers;
   this.maxHrlyCustomers = maxHrlyCustomers;
@@ -18,11 +19,9 @@ function cookieStand(name, minHrlyCustomers, maxHrlyCustomers, avgCookiesPerCust
   this.salesArr = [];
   this.customersPerHr = function() {
     return Math.floor(Math.random() * (this.maxHrlyCustomers - this.minHrlyCustomers + 1) + this.minHrlyCustomers);
-    console.log(customersPerHr);
   };
 
   this.dailyCookies = function() {
-    this.customersPerHr();
     for (var i = 0; i < storeHrs.length - 1; i++) {
       var cookiesPerHr = Math.floor(this.customersPerHr() * this.avgCookiesPerCust);
       this.salesArr.push(cookiesPerHr);
@@ -33,8 +32,7 @@ function cookieStand(name, minHrlyCustomers, maxHrlyCustomers, avgCookiesPerCust
   this.tableData = function() {
     this.dailyCookies();
     var table = document.getElementsByTagName('table')[0];
-    var tableBody = document.createElement('tbody');
-    table.appendChild(tableBody);
+    var tableBody = document.getElementsByTagName('tbody')[0];
     var tRow = document.createElement('tr');
     tableBody.appendChild(tRow);
     var nameTD = document.createElement('td');
@@ -46,6 +44,7 @@ function cookieStand(name, minHrlyCustomers, maxHrlyCustomers, avgCookiesPerCust
       tRow.appendChild(newTD);
     }
   };
+  allStores.push(this);
 }
 
 function createTable() {
@@ -55,6 +54,8 @@ function createTable() {
   table.appendChild(tableHead);
   var tRow = document.createElement('tr');
   tableHead.appendChild(tRow);
+  var tableBody = document.createElement('tbody');
+  table.appendChild(tableBody);
   for (var i = 0; i < storeHrs.length; i++) {
     var th = document.createElement('th');
     th.innerText = storeHrs[i];
@@ -63,8 +64,7 @@ function createTable() {
 };
 createTable();
 
-oneAndPike.tableData();
-seaTacAirport.tableData();
-seattleCenter.tableData();
-capitolHill.tableData();
-alki.tableData();
+for (var i = 0; i < allStores.length; i++) {
+  var aStore = allStores[i];
+  aStore.tableData();
+}
